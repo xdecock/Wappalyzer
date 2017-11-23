2100ba340e9f3204252edc9706a894a6d10d7647
De Cock Xavier <xdecock@gmail.com>
2017-11-23 17:58:52 +0100

    Test

--- src/wappalyzer.js
+++ src/wappalyzer.js
@@ -492,7 +492,6 @@ class Wappalyzer {
    */
   analyzeRobotsTxt(app, robotsTxt) {
     var patterns = this.parsePatterns(app.props.robotsTxt);
-
     if ( patterns.length ) {
       patterns.forEach(pattern => {
         if ( pattern.regex.test(robotsTxt) ) {
